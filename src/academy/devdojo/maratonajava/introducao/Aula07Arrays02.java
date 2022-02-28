fa04604583d6b7a16255690885e1ff98c6f4172a
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-02-28 02:28:14 -0300

    34 - Arrays pt 03

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -2,14 +2,22 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrays02 {
     public static void main(String[] args) {
-        // byte, short, int, long, float e double 0
-        // char '\u000' ' '
-        // boolean false
-        // String null
+        /*
+        byte, short, int, long, float e double 0
+        char "\u000"
+        boolean false
+        String null
+         */
 
-        String[] idades = new String[3]; // Tipo reference
-        System.out.println(idades[0]);
-        System.out.println(idades[1]);
-        System.out.println(idades[2]);
+
+        String[] nomes = new String[4]; // Tipo reference
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+        nomes[2] = "Hinata";
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(nomes[i]);
+        }
     }
 }
