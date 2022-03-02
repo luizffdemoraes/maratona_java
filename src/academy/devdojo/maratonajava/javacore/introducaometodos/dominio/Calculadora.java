9dfec1c3e9c7ace877807030d52fb63933de0ab6
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-03-02 13:04:21 -0300

    44 - Orientação Objetos - Métodos pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.introducaometodos.dominio;
+
+public class Calculadora {
+    // Não vai ter retorno.
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDoisNumeros() {
+        System.out.println(21 - 2);
+    }
+}
