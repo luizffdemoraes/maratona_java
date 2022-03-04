9e856950c200da2d0a520ff86f2febd969fd4a32
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-03-03 23:38:03 -0300

    47 - Orientação Objetos - Métodos pt 04 - Retorno pt 02

--- src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
@@ -21,4 +21,19 @@ public class Calculadora {
         }
         return num1 / num2;
     }
+
+    public double divideDoisNumeros02(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            return 0;
+        }
+    }
+
+    public void imprimeDivisaoDeDoisNumeros02(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Não existe divisão por zero");
+        }
+        System.out.println(num1/num2);
+    }
 }
