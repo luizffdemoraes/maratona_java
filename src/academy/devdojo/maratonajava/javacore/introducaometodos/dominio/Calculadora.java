4880e7ac179260f31e63200a4e4d7d2dad07d8b2
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-03-03 22:43:47 -0300

    45 - Orientação Objetos - Métodos pt 02 - Parâmetros

--- src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
@@ -9,4 +9,13 @@ public class Calculadora {
     public void subtraiDoisNumeros() {
         System.out.println(21 - 2);
     }
+
+    // Dois parametros
+    public void multiplicaDoisNumeros(int num1, float num2) {
+        System.out.println(num1 * num2);
+    }
+
+    public double divideDoisNumeros(double num1, double num2){
+        return 0;
+    }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.introducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.introducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        // Argumentos
+        calculadora.multiplicaDoisNumeros(10, 20.5F);
+    }
+}
